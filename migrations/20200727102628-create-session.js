91d9199887f1b356b1d72c88ac5f883fdc2f2bb5
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-08-09 00:09:52 +0530

    changed tables names to all camelacase

--- migrations/20200727102628-create-session.js
+++ migrations/20200727102628-create-session.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('sessions', {
+    return queryInterface.createTable('Sessions', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('sessions');
+    return queryInterface.dropTable('Sessions');
   }
 };
--- migrations/20200727214756-create-session-user.js
+++ migrations/20200727214756-create-session-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('sessionusers', {
+    return queryInterface.createTable('SessionUsers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('sessionusers');
+    return queryInterface.dropTable('SessionUsers');
   }
 };
