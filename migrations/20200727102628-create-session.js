928377a41e6edb632441a9da1db6cadb3822d7f5
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-08-08 23:44:25 +0530

    changed tables names to all lowercase

--- migrations/20200727102628-create-session.js
+++ migrations/20200727102628-create-session.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Sessions', {
+    return queryInterface.createTable('sessions', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Sessions');
+    return queryInterface.dropTable('sessions');
   }
 };
--- migrations/20200727214756-create-session-user.js
+++ migrations/20200727214756-create-session-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('sessionUsers', {
+    return queryInterface.createTable('sessionusers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('sessionUsers');
+    return queryInterface.dropTable('sessionusers');
   }
 };
